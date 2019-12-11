d532e9f56cbca288c4b62159acd6548a49d24a6a
jakemraz <jakemraz100@gmail.com>
2019-12-11 19:01:01 +0900

    ddb: change table location to constructor

--- ddb.py
+++ ddb.py
@@ -4,9 +4,10 @@ class Ddb:
     def __init__(self, table_name):
         self.table_name = table_name
         self.dynamodb = boto3.resource('dynamodb')
+        self.table = self.dynamodb.Table(table_name)
     def put_item(self, item):
-        table = self.dynamodb.Table(self.table_name)
-        response = table.put_item(
+        
+        response = self.table.put_item(
             Item = item
         )
 
